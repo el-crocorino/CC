40bbd0adf075cac6642aca585c70499002297547
Olivier Wenzek <owenzek@gmail.com>
2017-07-14 20:37:43 +0200

    Added Trip Model, updated index view, added basis for other models

--- CasseCroutards/main_app/models.py
+++ CasseCroutards/main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Trip( models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateField()
+    city_start = models.CharField( max_length = 100)
+    city_end = models.CharField( max_length = 100)
+    max_amount = models.DecimalField( max_digits = 6, decimal_places = 2)
+    created = models.DateTimeField( auto_now_add = True)
+    updated = models.DateTimeField( auto_now_add = True)
+    
+class Order( models.Model):
+    user = models.ForeignKey(User)
+    
+
+class Item( models.Model):
+    order = models.ForeignKey(Order)
+    
+
+class Appointment( models.Model):
+    order = models.ForeignKey(Order)
+    
+    
