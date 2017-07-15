fcc4bf070bf27aeb6bbe0a9baae1a8c1612228f7
Olivier Wenzek <owenzek@gmail.com>
2017-07-15 14:14:25 +0200

    Added add trip form on index page

--- CasseCroutards/main_app/urls.py
+++ CasseCroutards/main_app/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     url( r'^trip/([0-9]+)/$', views.trip_detail, name = 'trip detail'), 
+    url( r'^trip/new/$', views.trip_add, name = 'trip add'), 
+    url( r'^trip/delete/$', views.trip_delete, name = 'trip delete'), 
+    url( r'^trip/update/$', views.trip_update, name = 'trip update'), 
     url( r'^$', views.index, name = 'index'), 
 ]
