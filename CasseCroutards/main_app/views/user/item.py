b2548fcb1dc6bef7c255e8cb550bc7322be8fa44
Olivier Wenzek <owenzek@gmail.com>
2017-07-30 16:18:08 +0200

    Updated list templates

--- CasseCroutards/main_app/views/user/item.py
+++ CasseCroutards/main_app/views/user/item.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 
+from main_app.models import Trip, Order
+
 def user_item( pRequest, pUserId):
     '''
     Detailed user view
@@ -8,4 +10,13 @@ def user_item( pRequest, pUserId):
     @param int pUserId User id
     '''
     user = get_user_model().objects.get( id = pUserId)
-    return render(pRequest, 'user/item.html', {'user': user})
+    trips = Trip.objects.filter( user = user)
+    orders = Order.objects.filter( user = user)
+    return render(pRequest, 'user/item.html', {
+        'user': user, 
+        'trips': trips, 
+        'orders': orders, 
+        'showTripUser': False, 
+        'showOrderUser': False,
+        'showOrderTrip': True
+        })
