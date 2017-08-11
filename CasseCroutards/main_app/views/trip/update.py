0389e208b6c82629597aa7db3c757da71545c0a6
Olivier Wenzek <owenzek@gmail.com>
2017-08-11 07:45:46 +0200

    Added basic tripItem update logic, TODO : check if tripItem already exists

--- CasseCroutards/main_app/views/trip/update.py
+++ CasseCroutards/main_app/views/trip/update.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from main_app.models import Trip
-from main_app.forms import TripForm
+from main_app.models import Trip, TripItem
+from main_app.forms import TripForm, TripItemForm
 
 @login_required
 def trip_update( pRequest, pTripId):
@@ -10,28 +10,72 @@ def trip_update( pRequest, pTripId):
     Trip udpate view
     @param int pTripId Trip id
     '''
-
+    
     if pRequest.method == 'POST': 
-
-        form = TripForm( pRequest.POST)
-
-        if form.is_valid():
+        
+        tripForm = TripForm( pRequest.POST, prefix = "TripForm")
+        
+        if tripForm.is_valid():
 
             trip = Trip.objects.get( id = pTripId)
 
-            trip.date = form.cleaned_data['date']
-            trip.city_start = form.cleaned_data['city_start']
-            trip.city_end = form.cleaned_data['city_end']
-            trip.amount_limit = form.cleaned_data['amount_limit']
-            trip.participants_limit = form.cleaned_data['participants_limit']
-            trip.comment = form.cleaned_data['comment']
+            trip.date = tripForm.cleaned_data['date']
+            trip.city_start = tripForm.cleaned_data['city_start']
+            trip.city_end = tripForm.cleaned_data['city_end']
+            trip.amount_limit = tripForm.cleaned_data['amount_limit']
+            trip.participants_limit = tripForm.cleaned_data['participants_limit']
+            trip.comment = tripForm.cleaned_data['comment']
 
             trip.save()
+
+            tripItemCounter = tripForm.cleaned_data['tripItemCounter'] 
+            itemsSaved = True
+            tripItemIds = []
+
+            for i in range(0, tripItemCounter + 1):
+
+                itemSaved = False
+
+                if pRequest.POST['TripItemForm' + str(i) + '-title']: 
+            
+                    tripItemForm = TripItemForm( pRequest.POST, prefix = "TripItemForm" + str(i))
+                    tripItemForm.trip = trip
+                
+                    if tripItemForm.is_valid():
+
+                        #TODO : ajouter un check : si item a un id => il existe => le mettre à jour
+                        #s'il n'en a pas => il n'existe pas => le créer
+
+                        tripItem = tripItemForm.save( commit = False)
+                        tripItem.trip = trip
+                        tripItem.save()
+
+                        tripItemIds.append( tripItem.id)
+
+                        itemSaved = True
+
+                    itemsSaved = itemsSaved and itemSaved 
+            
+            if not itemsSaved:
+
+                trip.delete()
+
+                for itemId in tripItemIds:
+                    tripItem = TripItem.objects.get( id = itemId)
+                    tripItem.delete()
+
+
+
+
+
             return render(pRequest, 'trip/item.html', {'trip': trip})
 
     else:
+
         trip = Trip.objects.get( id = pTripId)
-        updateTripForm = TripForm( initial = {
+        trip.items = TripItem.objects.filter( trip = pTripId)
+        
+        updateTripForm = TripForm( prefix = 'TripForm',  initial = {
             'date' : trip.date, 
             'city_start' : trip.city_start, 
             'city_end' : trip.city_end, 
@@ -39,5 +83,25 @@ def trip_update( pRequest, pTripId):
             'participants_limit' : trip.participants_limit, 
             'comment' : trip.comment
             })
+
+        updateTripItemForms = []
         
-        return render(pRequest, 'trip/update.html', {'trip': trip,'updateTripForm' : updateTripForm}) 
+        for index, item in enumerate( trip.items, start = 0):
+            
+            updateTripItemForm = TripItemForm( prefix = 'TripItemForm' + str( index), initial = {
+                'title': item.title,
+                'description': item.description,
+                'average_value': item.average_value,
+                'average_qty': item.average_qty
+            })
+
+            updateTripItemForms.append( updateTripItemForm)
+        
+        addTripItemForm = TripItemForm( prefix = 'TripItemForm')
+
+        return render( pRequest, 'trip/update.html', {
+            'trip': trip,
+            'updateTripForm' : updateTripForm, 
+            'addTripItemForm' : addTripItemForm, 
+            'updateTripItemForms': updateTripItemForms
+        }) 
